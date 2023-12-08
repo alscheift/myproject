1505324a6ad842c4071fa529643f674fbc483c92
Afif Nur Fauzi <alscheift@gmail.com>
2023-12-08 19:17:49 +0700

    Install flowbite
    composer global require laravel/installer
    npm install -D tailwindcss postcss autoprefixer flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
 
